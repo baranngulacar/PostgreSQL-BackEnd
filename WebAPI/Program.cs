f8d13c5cdc6a82eae508a670c70caf10219bb72b
barangulacar <Barangulacar@hotmail.com>
2023-04-21 01:03:58 +0300

    mesaj

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,8 +8,11 @@ public class Program
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
+
         Host.CreateDefaultBuilder(args)
-        //.UseServiceProviderFactory
+
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+
         .ConfigureWebHostDefaults(webBuilder =>
         {
             webBuilder.UseStartup<Startup>();
