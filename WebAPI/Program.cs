530be3b095493192cfaa7747a248d846009aadb5
barangulacar <Barangulacar@hotmail.com>
2023-04-20 09:41:37 +0300

    first commit

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -9,7 +9,7 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        //.UseServiceProviderFactory
         .ConfigureWebHostDefaults(webBuilder =>
         {
             webBuilder.UseStartup<Startup>();
